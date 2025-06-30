2a739d29780cad3ac145933529cecfb52ecd7aa1
Ninadiantea <Ninadianthea@gmail.com>
2025-06-30 16:35:06 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+// Data sementara, gunakan database untuk produksi
+const nasabahList = [];
+const pinjamanList = [];
+const logPembayaran = [];
+
+module.exports = { nasabahList, pinjamanList, logPembayaran };
