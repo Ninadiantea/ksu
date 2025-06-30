4962487d1710569bff44b9a381af3fddb0cebd7f
Ninadiantea <Ninadianthea@gmail.com>
2025-06-30 16:34:09 +0700

    Create comfig.js

---
+++ comfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: 'ISI_TOKEN_BOT', // ganti dengan token bot kamu
+  GROUP_ID: -1001234567890,   // ganti dengan group id Telegram kamu
+  PETUGAS_NAMA: 'Dedi'        // ganti nama petugas sesuai wilayah
+};
